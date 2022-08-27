f44dd776c519dd25de6705e4d8923575c98ef8cb
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-08-27 12:44:15 +0700

    feat: add placeholder text on form inputs

--- pages/login.jsx
+++ pages/login.jsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 export default function Login() {
   return (
     <div className="h-screen bg-blue-main text-white px-10">
-      <div className="h-[300px]"></div>
+      <div className="h-[250px]"></div>
       <form className="flex flex-col  text-black gap-3">
         <div className="flex flex-col">
           <label htmlFor="email" className="text-white">
             Email
           </label>
-          <input type="email" name="email" className="border rounded" />
+          <input type="email" name="email" className="border rounded px-1" placeholder="email@example.com"/>
         </div>
         {/* <label htmlFor="phone-number" className="text-white">Phone Number</label>
         <input type="number" name="phone-number" className="border rounded"/> */}
@@ -17,7 +17,7 @@ export default function Login() {
           <label htmlFor="password" className="text-white">
             Password
           </label>
-          <input type="password" name="password" className="border rounded" />
+          <input type="password" name="password" className="border rounded px-1" placeholder="password" />
         </div>
       </form>
 
