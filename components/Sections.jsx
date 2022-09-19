a17e6d63494d664d3599eadb6024ca87abd6c7d5
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-09-19 19:38:27 +0700

    feat: changed Sections to use Swiper

--- components/Sections.jsx
+++ components/Sections.jsx
@@ -1,13 +1,30 @@
 import { Section } from "./Section"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { freeMode } from "swiper"
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 export const Sections = () => {
   return(
     <div className="w-full mt-16">
       <h1 className="ml-[5%]">Sections</h1>
-      <div className="sections overflow-x-scroll gap-52 whitespace-nowrap p-1 ml-[5%]">
-        <Section />
-        <Section />
-      </div>
+      <Swiper
+        modules={[freeMode]}
+        spaceBetween={1}
+        slidesPerView={1.8}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+        className="overflow-hidden flex items-center p-2"
+      >
+        <SwiperSlide className="flex justify-center p-2">
+          <Section />
+        </SwiperSlide>
+        <SwiperSlide className="flex p-2 justify-center">
+          <Section />
+        </SwiperSlide>
+      </Swiper >
     </div>
   )
 }
