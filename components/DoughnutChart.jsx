67073723dceb4eb61424b5817dd5719978f424f7
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-10-04 09:33:41 +0700

    feat: add Chart.JS
    - Installed ChartJS package.
    - Added a functional Doughnut Chart in Statistics page.

---
+++ components/DoughnutChart.jsx
@@ -0,0 +1,17 @@
+import { Doughnut } from "react-chartjs-2";
+import {Chart as ChartJS} from 'chart.js/auto'
+
+export const DoughnutChart = (props) => {
+  console.log(props);
+  if(props.data !== undefined) {
+    return <Doughnut data={props.data} options={{
+      plugins: {
+        legend: {
+          labels: {
+            color: 'white'
+          }
+        }
+      }
+    }}/>;
+  }
+}
