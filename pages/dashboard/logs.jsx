5869360a7b482f97ce8f40c83a1310bf6d25b3dc
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-10-15 08:49:19 +0700

    feat: add modal for new logs

--- pages/dashboard/logs.jsx
+++ pages/dashboard/logs.jsx
@@ -1,11 +1,20 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import UserContext from "../../utils/context/userContext";
 import { DashboardHeader } from "../../components/DashboardHeader";
+import Link from "next/link";
 
 export default function Logs() {
   const { user, setUser } = useContext(UserContext);
   const router = useRouter();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [logs, setLogs] = useState([]);
+  const [logTitle, setLogTitle] = useState("");
+  const [logAmount, setLogAmount] = useState(0);
+  const [logType, setLogType] = useState("expense");
+  const [logDate, setLogDate] = useState("");
+  
 
   //? Auth Protection
   useEffect(() => {
@@ -17,6 +26,11 @@ export default function Logs() {
     }
   }, []);
 
+  //? Toggle Modal
+  function toggleModal() {
+    setIsModalOpen(!isModalOpen);
+  }
+
   return (
     <main className="bg-blue-main flex items-center justify-center">
       <div className="flex flex-col w-full min-h-screen bg-blue-main items-center text-white font-Montserrat pb-10 sm:w-[500px] outline outline-white">
@@ -25,7 +39,78 @@ export default function Logs() {
         <div className="w-full h-[1px] bg-white mt-[35px] mb-[40px]"></div>
 
         <h1 className="text-xl">Logs</h1>
+
+        {/* Modal */}
+        {isModalOpen ? (
+          <div className="w-full h-screen fixed bg-slate-900 bg-opacity-80 flex justify-center items-center">
+            <div className="w-[80%] relative bg-blue-main rounded p-5">
+              <div
+                className="absolute top-0 right-0 m-3 w-[24px] text-center cursor-pointer"
+                onClick={() => toggleModal()}
+              >
+                X
+              </div>
+              <form className="flex flex-col items-center justify-center">
+                <h1 className="text-xl">Add a new log</h1>
+                <label htmlFor="log-title" className="mt-3">
+                  Title
+                </label>
+                <input
+                  type="text"
+                  name="log-title"
+                  id="log-title"
+                  className="w-[80%] rounded outline-none p-[2px] text-slate-900"
+                />
+
+                <label htmlFor="log-amount" className="mt-3">
+                  Amount
+                </label>
+                <div className="w-[80%] flex gap-2">
+                  <select className="rounded outline-none p-[4px] text-slate-900">
+                    <option value="spend">-</option>
+                    <option value="gain">+</option>
+                  </select>
+                  <input
+                    type="number"
+                    name="log-amount"
+                    id="log-amount"
+                    className="w-full rounded outline-none p-[2px] text-slate-900"
+                  />
+                </div>
+
+                <label htmlFor="log-type" className="mt-3">
+                  Type
+                </label>
+                <select
+                  name="log-type"
+                  id="log-type"
+                  className="w-[80%] rounded outline-none p-[2px] text-slate-900"
+                >
+                  <option value="Food">Food</option>
+                  <option value="Entertainment">Entertainment</option>
+                  <option value="Transportation">Transportation</option>
+                  <option value="Other">Other</option>
+                </select>
+                <button
+                  type="submit"
+                  className="w-[80%] mt-8 rounded py-1 bg-green-500"
+                >
+                  Add
+                </button>
+              </form>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+
         <p>Coming soon...</p>
+        <div
+          className="bg-blue-button fixed bottom-0 right-0 w-[60px] rounded-full cursor-pointer aspect-square m-5 flex items-center justify-center text-4xl"
+          onClick={() => toggleModal()}
+        >
+          +
+        </div>
       </div>
     </main>
   );
